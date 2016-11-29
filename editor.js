a9b1278f51973301cee6cd84766daf07ed3a9afb
Luca <lcpp1994@gmail.com>
2016-11-29 20:14:43 +0100

    added calculatePitch

--- editor.js
+++ editor.js
@@ -1,11 +1,16 @@
 $(document).ready(function () {
+    var VF = Vex.Flow;
     var canvas = document.getElementById("score");
-    var renderer = new Vex.Flow.Renderer(canvas, Vex.Flow.Renderer.Backends.SVG);
+    var renderer = new VF.Renderer(canvas, Vex.Flow.Renderer.Backends.SVG);
     var ctx = renderer.getContext();
-    var bassoVoice = new Array();
-    var tenoreVoice = new Array();
-    var altoVoice = new Array();
-    var sopranoVoice = new Array();
+    var bassoNotes = new Array();
+    var tenoreNotes = new Array();
+    var altoNotes = new Array();
+    var sopranoNotes = new Array();
+    var bassoVoice = new VF.Voice({num_beats: 4,  beat_value: 4});
+    var tenoreVoice = new VF.Voice({num_beats: 4,  beat_value: 4});
+    var altoVoice = new VF.Voice({num_beats: 4,  beat_value: 4});
+    var sopranoVoice = new VF.Voice({num_beats: 4,  beat_value: 4});
     var staff,
         formatter,
         voice,
@@ -18,7 +23,17 @@ $(document).ready(function () {
     processStaves();
     drawStaves();
     canvas.addEventListener("click", addNote, false);
-    alert(trebleStave.getBottomLineY());
+    //alert(bassStave.getBottomLineY());
+    altoNotes.push(new Vex.Flow.StaveNote({clef: "treble", keys: ["c/3", "e/4", "g/4"], duration: "h" }));
+    altoVoice.setStrict(false);
+    altoVoice.addTickables(altoNotes);
+    var formatter = new VF.Formatter().joinVoices([altoVoice]).format([altoVoice], 400);
+    altoVoice.draw(ctx, trebleStave);
+    bassoNotes.push(new Vex.Flow.StaveNote({clef: "bass", keys: ["c/3", "e/4", "g/4"], duration: "q" }));
+    bassoVoice.setStrict(false);
+    bassoVoice.addTickables(bassoNotes);
+    var formatter = new VF.Formatter().joinVoices([bassoVoice]).format([bassoVoice], 400);
+    bassoVoice.draw(ctx, bassStave);
 
     function processStaves() {
 
@@ -33,9 +48,8 @@ $(document).ready(function () {
         }
         renderer.resize(staveSize, 500);
 
-        //TODO bassStave position relative to trebleStave
-        trebleStave = new Vex.Flow.Stave(10, 20, staveSize);
-        bassStave = new Vex.Flow.Stave(10, 100, staveSize);
+        trebleStave = new VF.Stave(10, 20, staveSize);
+        bassStave = new VF.Stave(10, trebleStave.getBottomLineY() + 10, staveSize);
         trebleStave.addClef("treble").addTimeSignature(numBeats + "/" + beatValue);
         bassStave.addClef("bass").addTimeSignature(numBeats + "/" + beatValue);
 
@@ -51,7 +65,7 @@ $(document).ready(function () {
         bassStave.setContext(ctx).draw();
     }
 
-    //returns the radio element selected with the given name
+    //return the radio element selected with the given name
     function getRadioSelected(name) {
         var elements = document.getElementsByName(name);
         for (i = 0; i < elements.length; i++) {
@@ -60,38 +74,52 @@ $(document).ready(function () {
         }
     }
 
+    //check if there are no notes to display
     function isStaveEmpty() {
-        if (bassoVoice.length <= 0 && tenoreVoice.length <= 0 && tenoreVoice.length <= 0 && altoVoice.length <= 0)
+        if (bassoNotes.length <= 0 && tenoreNotes.length <= 0 && tenoreNotes.length <= 0 && altoNotes.length <= 0)
             return true;
         return false;
     }
 
-    //returns the length of the longest voice
+    //return the length of the longest voice
     function maxNotes() {
-        return Math.max(bassoVoice.length, tenoreVoice.length, altoVoice.length, sopranoVoice.length);
+        return Math.max(bassoNotes.length, tenoreNotes.length, altoNotes.length, sopranoNotes.length);
     }
 
+    //add the note to the stave
     function addNote(e) {
-        var x, y;
-
-        if (e.pageX != undefined && e.pageY != undefined) {
-            x = e.pageX;
-            y = e.pageY;
-        }
-        else {
-            x = e.clientX + document.body.scrollLeft +
-                document.documentElement.scrollLeft;
-            y = e.clientY + document.body.scrollTop +
-                document.documentElement.scrollTop;
-        }
-
-
-        var rect = canvas.getBoundingClientRect();
-
-        x = e.clientX - rect.left;
-        y = e.clientY - rect.top;
-        alert(x + " " + y);
+        var duration = getRadioSelected("notes");
+        var accidental = getRadioSelected("accidental");
+        var tone = getRadioSelected("tone");
+        var pitch = calculatePitch(e, tone);
     }
 
+    //calculate the pitch based on the mouse click position
+    function calculatePitch(e, tone) {
+        var rect = canvas.getBoundingClientRect();
+        var x = e.clientX - rect.left;
+        var y = e.clientY - rect.top;
+        var trebleBottom = trebleStave.getBottomLineY();
+        var bassBottom = bassStave.getBottomLineY();
+        if(tone == "basso") {
+            if(y <= bassBottom + 5 && y >= bassBottom - 65) {
 
+            }
+        }
+        else if(tone == "tenore") {
+            if(y <= bassBottom - 20 && y >= bassBottom - 80) {
+
+            }
+        }
+        else if(tone == "alto") {
+            if(y <= trebleBottom + 20 && y >= trebleBottom - 40) {
+
+            }
+        }
+        else if(tone == "soprano") {
+            if(y <= trebleBottom && y >= trebleBottom - 60) {
+
+            }
+        }
+    }
 });
