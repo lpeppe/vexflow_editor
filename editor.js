6a7244c775135889f16378d1dd482a6d35207420
lpeppe <lcpp1994@gmail.com>
2016-11-29 16:54:40 +0100

    added some functions

--- editor.js
+++ editor.js
@@ -2,8 +2,10 @@ $(document).ready(function () {
     var canvas = document.getElementById("score");
     var renderer = new Vex.Flow.Renderer(canvas, Vex.Flow.Renderer.Backends.SVG);
     var ctx = renderer.getContext();
-    var trebleNotes = new Array();
-    var bassNotes = new Array();
+    var bassoVoice = new Array();
+    var tenoreVoice = new Array();
+    var altoVoice = new Array();
+    var sopranoVoice = new Array();
     var staff,
         formatter,
         voice,
@@ -15,27 +17,25 @@ $(document).ready(function () {
         cursorHeight = 150;
     processStaves();
     drawStaves();
-
+    canvas.addEventListener("click", addNote, false);
+    alert(trebleStave.getBottomLineY());
 
     function processStaves() {
 
         var staveSize;
 
         // set stave width
-        if (trebleNotes.length < 6) {
+        if (isStaveEmpty())
             staveSize = 930;
-        }
         else {
             // about 85 pixels per note
-            staveSize = (trebleNotes.length + 1) * 85;
+            staveSize = (maxNotes() + 1) * 85;
         }
         renderer.resize(staveSize, 500);
+
         //TODO bassStave position relative to trebleStave
         trebleStave = new Vex.Flow.Stave(10, 20, staveSize);
         bassStave = new Vex.Flow.Stave(10, 100, staveSize);
-
-        //alert(numBeats);
-        timeSign = String(numBeats) + "/" + String(beatValue);
         trebleStave.addClef("treble").addTimeSignature(numBeats + "/" + beatValue);
         bassStave.addClef("bass").addTimeSignature(numBeats + "/" + beatValue);
 
@@ -50,4 +50,48 @@ $(document).ready(function () {
         trebleStave.setContext(ctx).draw();
         bassStave.setContext(ctx).draw();
     }
+
+    //returns the radio element selected with the given name
+    function getRadioSelected(name) {
+        var elements = document.getElementsByName(name);
+        for (i = 0; i < elements.length; i++) {
+            if (elements[i].checked)
+                return elements[i].id;
+        }
+    }
+
+    function isStaveEmpty() {
+        if (bassoVoice.length <= 0 && tenoreVoice.length <= 0 && tenoreVoice.length <= 0 && altoVoice.length <= 0)
+            return true;
+        return false;
+    }
+
+    //returns the length of the longest voice
+    function maxNotes() {
+        return Math.max(bassoVoice.length, tenoreVoice.length, altoVoice.length, sopranoVoice.length);
+    }
+
+    function addNote(e) {
+        var x, y;
+
+        if (e.pageX != undefined && e.pageY != undefined) {
+            x = e.pageX;
+            y = e.pageY;
+        }
+        else {
+            x = e.clientX + document.body.scrollLeft +
+                document.documentElement.scrollLeft;
+            y = e.clientY + document.body.scrollTop +
+                document.documentElement.scrollTop;
+        }
+
+
+        var rect = canvas.getBoundingClientRect();
+
+        x = e.clientX - rect.left;
+        y = e.clientY - rect.top;
+        alert(x + " " + y);
+    }
+
+
 });
