5d108c51021d5b092041991568fb6b561192683b
Luca <lcpp1994@gmail.com>
2016-12-05 20:16:07 +0100

    now adding the notes works properly on the different voices. Cleaned the code

--- src/editor.js
+++ src/editor.js
@@ -6,94 +6,31 @@ function render() {
     timeSign = getRadioSelected("time");
     beatNum = timeSign.split("/")[0];
     beatValue = timeSign.split("/")[1];
-    /*var bassoNotes = new Array();
-    var tenoreNotes = new Array();
-    var altoNotes = new Array();
-    var sopranoNotes = new Array();*/
+
     var measures = [];
     var curIndex = 0;
-    //TODO parse num_beats and beat_value from the selected radio button
-    /*var bassoVoice = new VF.Voice({num_beats: 4, beat_value: 4});
-    var tenoreVoice = new VF.Voice({num_beats: 4, beat_value: 4});
-    var altoVoice = new VF.Voice({num_beats: 4, beat_value: 4});
-    var sopranoVoice = new VF.Voice({num_beats: 4, beat_value: 4});*/
-
-    /*processStaves();
-    drawStaves();*/
     init();
-    measures[0].addNote(new Vex.Flow.StaveNote({clef: "treble", keys: ["a/4"], duration: "h"}), "basso");
-    alert(measures[0].isComplete("basso"));
-    //altoVoice.setStrict(true);
-    //altoVoice.setMode(3);
-    //altoNotes.push(//new Vex.Flow.StaveNote({clef: "treble", keys: ["a/4"], duration: "h"}),
-        //new Vex.Flow.StaveNote({clef: "treble", keys: ["a/4"], duration: "h"}),
-        //new Vex.Flow.StaveNote({clef: "treble", keys: ["a/4"], duration: "h"}));
-    //altoVoice.setStrict(false);
-
-    //alert(altoVoice.isComplete());
-    /*altoVoice.addTickables(altoNotes);
-    var formatter = new VF.Formatter().joinVoices([altoVoice]).format([altoVoice], 400);
-    altoVoice.draw(ctx, trebleStave);
-    bassoNotes.push(new Vex.Flow.StaveNote({clef: "bass", keys: ["d/3"], duration: "q"}));
-    bassoVoice.setStrict(false);
-    bassoVoice.addTickables(bassoNotes);
-    var formatter = new VF.Formatter().joinVoices([bassoVoice]).format([bassoVoice], 400);
-    bassoVoice.draw(ctx, bassStave);*/
 
 
     function init() {
         measures.push(new Measure(0));
-        measures[0].render(10);
         measures.push(new Measure(1));
-        measures[1].render(360);
         measures.push(new Measure(2));
-        measures[2].render(660);
-        renderer.resize(measures.length * 300 + 60, 500);
+        measures.push(new Measure(3));
+        renderMeasures();
         canvas.addEventListener("click", addNote, false);
     }
 
-    //render the staves
-    function processStaves() {
-
-        var staveSize;
-
-        // set stave width
-        if (isStaveEmpty() || maxNotes() < 10)
-            staveSize = 930;
-        else {
-            // about 85 pixels per note
-            staveSize = (maxNotes() + 1) * 85;
+    //renders all the measures
+    function renderMeasures() {
+        ctx.clear();
+        renderer.resize(measures.length * 300 + 60, 500);
+        for(i = 0; i < measures.length; i++) {
+            if(i == 0)
+                measures[i].render(10);
+            else
+                measures[i].render(measures[i-1].getEndX());
         }
-        renderer.resize(staveSize, 500);
-
-        trebleStave = new VF.Stave(10, 20, staveSize);
-        bassStave = new VF.Stave(10, trebleStave.getBottomLineY() + 10, staveSize);
-        trebleStave.addClef("treble").addTimeSignature(timeSign);
-        bassStave.addClef("bass").addTimeSignature(timeSign);
-
-        // add key
-        var keySign = $("#ks :selected").text();
-
-        trebleStave.addKeySignature(keySign);
-        bassStave.addKeySignature(keySign);
-    }
-
-    //draw the staves
-    function drawStaves() {
-        trebleStave.setContext(ctx).draw();
-        bassStave.setContext(ctx).draw();
-    }
-
-    //check if there are no notes to display
-    function isStaveEmpty() {
-        if (bassoNotes.length <= 0 && tenoreNotes.length <= 0 && tenoreNotes.length <= 0 && altoNotes.length <= 0)
-            return true;
-        return false;
-    }
-
-    //return the length of the longest voice
-    function maxNotes() {
-        return Math.max(bassoNotes.length, tenoreNotes.length, altoNotes.length, sopranoNotes.length);
     }
 
     //add the note to the stave
@@ -102,24 +39,20 @@ function render() {
         var accidental = getRadioSelected("accidental");
         var voice = getRadioSelected("voice");
         var pitch = calculatePitch(e, voice);
-        switch(voice) {
-            case "basso":
-                bassoNotes.push(new Vex.Flow.StaveNote({clef: "bass", keys: [pitch], duration: duration}));
+        //TODO if all the measure's voices are complete create a new measure
+        for(i = 0; i < measures.length; i++) {
+            if(measures[i].isComplete(voice) == false) {
+                if(voice == "basso" || voice == "tenore")
+                    measures[i].addNote(new Vex.Flow.StaveNote({clef: "bass", keys: [pitch], duration: duration}), voice);
+                else
+                    measures[i].addNote(new Vex.Flow.StaveNote({clef: "treble", keys: [pitch], duration: duration}), voice);
                 break;
-            case "tenore":
-                tenoreNotes.push(new Vex.Flow.StaveNote({clef: "bass", keys: [pitch], duration: duration}));
-                break;
-            case "alto":
-                altoNotes.push(new Vex.Flow.StaveNote({clef: "treble", keys: [pitch], duration: duration}));
-                break;
-            case "soprano":
-                sopranoNotes.push(new Vex.Flow.StaveNote({clef: "treble", keys: [pitch], duration: duration}));
-                break;
+            }
         }
         ctx.clear();
-        processStaves();
-        drawStaves();
-        processVoices();
+        renderMeasures();
+        for(i = 0; i < measures.length; i++)
+            measures[i].drawNotes();
     }
 
     //calculate the pitch based on the mouse click position
@@ -133,39 +66,39 @@ function render() {
             y = y - diff;
         else
             y = y*1 + (5 - diff);
-        var trebleBottom = trebleStave.getBottomLineY();
-        var bassBottom = bassStave.getBottomLineY();
+        var trebleBottom = measures[0].getStaveBottom("treble");
+        var bassBottom = measures[0].getStaveBottom("bass");
         if (tone == "basso") {
             if (y <= bassBottom && y >= bassBottom - 60) {
                 //first note e/2, last note c/4 on the bass stave
-                return getNote(y, bassStave);
+                return getNote(y, bassBottom, "bass");
             }
             return;
         }
         else if (tone == "tenore") {
             if (y <= bassBottom - 25 && y >= bassBottom - 80) {
                 //first note c/3, last note g/4 on the bass stave
-                return getNote(y, bassStave);
+                return getNote(y, bassBottom, "bass");
             }
             return;
         }
         else if (tone == "alto") {
             if (y <= trebleBottom + 15 && y >= trebleBottom - 35) {
                 //first note g/3, last note c/5 on the treble stave
-                return getNote(y, trebleStave);
+                return getNote(y, trebleBottom, "treble");
             }
             return;
         }
         else if (tone == "soprano") {
             if (y <= trebleBottom && y >= trebleBottom - 60) {
                 //first note c/4, last note a/5 on the treble stave
-                return getNote(y, trebleStave);
+                return getNote(y, trebleBottom, "treble");
             }
             return;
         }
     }
 
-    function getNote(y, stave) {
+    function getNote(y, staveBottom, stave) {
         var octave;
         var note;
         var bottom;
@@ -174,13 +107,13 @@ function render() {
             y = y - diff;
         else
             y += Number((5 - diff));
-        if (stave === trebleStave) {
-            bottom = trebleStave.getBottomLineY() + 15;
+        if (stave == "treble") {
+            bottom = staveBottom + 15;
             note = 4; //c is 0, b is 6
             octave = 3;
         }
-        else if (stave === bassStave) {
-            bottom = bassStave.getBottomLineY();
+        else if (stave == "bass") {
+            bottom = staveBottom;
             note = 2; //c is 0, b is 6
             octave = 2;
         }
@@ -212,52 +145,6 @@ function render() {
         }
     }
 
-    function processVoices() {
-        bassoVoice = new VF.Voice({num_beats: 4, beat_value: 4, resolution: Vex.Flow.RESOLUTION});
-        tenoreVoice = new VF.Voice({num_beats: 4, beat_value: 4, resolution: Vex.Flow.RESOLUTION});
-        altoVoice = new VF.Voice({num_beats: 4, beat_value: 4, resolution: Vex.Flow.RESOLUTION});
-        sopranoVoice = new VF.Voice({num_beats: 4, beat_value: 4, resolution: Vex.Flow.RESOLUTION});
-        bassoVoice.setStrict(false);
-        tenoreVoice.setStrict(false);
-        altoVoice.setStrict(false);
-        sopranoVoice.setStrict(false);
-        bassoVoice.addTickables(bassoNotes);
-        tenoreVoice.addTickables(tenoreNotes);
-        altoVoice.addTickables(altoNotes);
-        sopranoVoice.addTickables(sopranoNotes);
-        var formatter = new VF.Formatter();
-        if(bassoNotes.length != 0 && tenoreNotes.length != 0) {
-            formatter.joinVoices([bassoVoice, tenoreVoice]).format([bassoVoice, tenoreVoice], 400);
-            bassoVoice.draw(ctx, bassStave);
-            tenoreVoice.draw(ctx, bassStave);
-        }
-        else if(bassoNotes.length == 0) {
-            formatter.joinVoices([tenoreVoice]).format([tenoreVoice], 400);
-            tenoreVoice.draw(ctx, bassStave);
-        }
-        else {
-            formatter.joinVoices([bassoVoice]).format([bassoVoice], 400);
-            bassoVoice.draw(ctx, bassStave);
-        }
-        if(sopranoNotes.length != 0 && altoNotes.length != 0) {
-            formatter.joinVoices([sopranoVoice, altoVoice]).format([sopranoVoice, altoVoice], 400);
-            sopranoVoice.draw(ctx, trebleStave);
-            altoVoice.draw(ctx, trebleStave);
-        }
-        else if(sopranoNotes.length == 0) {
-            formatter.joinVoices([altoVoice]).format([altoVoice], 400);
-            altoVoice.draw(ctx, trebleStave);
-        }
-        else {
-            formatter.joinVoices([sopranoVoice]).format([sopranoVoice], 400);
-            sopranoVoice.draw(ctx, trebleStave);
-        }
-        //formatter.joinVoices([sopranoVoice, altoVoice]).format([sopranoVoice, altoVoice], 400);
-        /*bassoVoice.draw(ctx, bassStave);
-        tenoreVoice.draw(ctx, bassStave);
-        sopranoVoice.draw(ctx, trebleStave);
-        altoVoice.draw(ctx, trebleStave);*/
-    }
 }
 
 //return the radio element selected with the given name
--- src/measure.js
+++ src/measure.js
@@ -13,30 +13,37 @@ function Measure(index) {
     this.tenoreVoice.setMode(3);
     this.sopranoVoice.setMode(3);
     this.altoVoice.setMode(3);
+    this.formatter = new VF.Formatter();
 }
 
 Measure.prototype.getIndex = function () {
     return index;
 }
 
-//adds a note in the measure TODO: add notes to the voices
+//adds a note in the measure
 Measure.prototype.addNote = function (note, voiceName) {
     switch(voiceName) {
         case "basso":
-            this.bassoNotes.push(new Vex.Flow.StaveNote({clef: "bass", keys: [pitch], duration: duration}));
-            this.bassoVoice.addTickables(this.bassoNotes);
+            //this.bassoNotes.push(note);
+            //this.bassoVoice.addTickables(this.bassoNotes);
+            this.bassoNotes.push(note);
+            this.bassoVoice.addTickable(note);
+            //this.drawNotes(this.bassoVoice, this.bassStave);
             break;
         case "tenore":
-            this.tenoreNotes.push(new Vex.Flow.StaveNote({clef: "bass", keys: [pitch], duration: duration}));
-            this.tenoreVoice.addTickables(this.tenoreNotes);
+            this.tenoreNotes.push(note);
+            this.tenoreVoice.addTickable(note);
+            //this.drawNotes(this.tenoreVoice, this.bassStave);
             break;
         case "alto":
-            this.altoNotes.push(new Vex.Flow.StaveNote({clef: "treble", keys: [pitch], duration: duration}));
-            this.altoVoice.addTickables(this.altoNotes);
+            this.altoNotes.push(note);
+            this.altoVoice.addTickable(note);
+            //this.drawNotes(this.altoVoice, this.trebleStave);
             break;
         case "soprano":
-            this.sopranoNotes.push(new Vex.Flow.StaveNote({clef: "treble", keys: [pitch], duration: duration}));
-            this.sopranoVoice.addTickables(this.sopranoNotes);
+            this.sopranoNotes.push(note);
+            this.sopranoVoice.addTickable(note);
+            //this.drawNotes(this.sopranoVoice, this.trebleStave);
             break;
     }
 }
@@ -46,17 +53,17 @@ Measure.prototype.render = function (x) {
     var staveSize = 300;
     if(this.index == 0)
         staveSize = 350;
-    var trebleStave = new VF.Stave(x, 20, staveSize);
-    var bassStave = new VF.Stave(x, trebleStave.getBottomLineY() + 10, staveSize);
+    this.trebleStave = new VF.Stave(x, 20, staveSize);
+    this.bassStave = new VF.Stave(x, this.trebleStave.getBottomLineY() + 10, staveSize);
     if(this.index == 0) {
-        trebleStave.addClef("treble").addTimeSignature(timeSign);
-        bassStave.addClef("bass").addTimeSignature(timeSign);
+        this.trebleStave.addClef("treble").addTimeSignature(timeSign);
+        this.bassStave.addClef("bass").addTimeSignature(timeSign);
         var keySign = $("#ks :selected").text();
-        trebleStave.addKeySignature(keySign);
-        bassStave.addKeySignature(keySign);
+        this.trebleStave.addKeySignature(keySign);
+        this.bassStave.addKeySignature(keySign);
     }
-    trebleStave.setContext(ctx).draw();
-    bassStave.setContext(ctx).draw();
+    this.trebleStave.setContext(ctx).draw();
+    this.bassStave.setContext(ctx).draw();
 }
 
 Measure.prototype.resize = function (size) {
@@ -79,5 +86,29 @@ Measure.prototype.isComplete = function (voiceName) {
             return this.sopranoVoice.isComplete();
         case "alto":
             return this.altoVoice.isComplete();
+    }
+}
+
+Measure.prototype.getEndX = function () {
+    return this.trebleStave.getX() + this.trebleStave.getWidth();
+}
+
+Measure.prototype.drawNotes = function () {
+    this.formatter.joinVoices([this.bassoVoice]).format([this.bassoVoice], this.trebleStave.getWidth());
+    this.formatter.joinVoices([this.tenoreVoice]).format([this.tenoreVoice], this.trebleStave.getWidth());
+    this.formatter.joinVoices([this.altoVoice]).format([this.altoVoice], this.trebleStave.getWidth());
+    this.formatter.joinVoices([this.sopranoVoice]).format([this.sopranoVoice], this.trebleStave.getWidth());
+    this.bassoVoice.draw(ctx, this.bassStave);
+    this.tenoreVoice.draw(ctx, this.bassStave);
+    this.altoVoice.draw(ctx, this.trebleStave);
+    this.sopranoVoice.draw(ctx, this.trebleStave);
+}
+
+Measure.prototype.getStaveBottom = function(stave) {
+    switch(stave){
+        case "bass":
+            return this.bassStave.getBottomLineY();
+        case "treble":
+            return this.trebleStave.getBottomLineY();
     }
 }
